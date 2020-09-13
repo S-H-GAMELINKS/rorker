c10b954d52c5a2cf7806d5545ffff867509b039d
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-09-14 01:35:28 +0900

    add test_rorker_run

--- test/rorker_test.rb
+++ test/rorker_test.rb
@@ -12,4 +12,16 @@ class RorkerTest < Minitest::Test
     rorker = Rorker.new(task)
     assert_kind_of Rorker, rorker
   end
+
+  def test_rorker_run
+    task = Ractor.new{
+      Ractor.recv
+    }
+    rorker = Rorker.new(task)
+    rorker.send(42)
+    rorker.run{|worker, result|
+      assert_kind_of Ractor, worker
+      assert_equal 42, result
+    }
+  end
 end
