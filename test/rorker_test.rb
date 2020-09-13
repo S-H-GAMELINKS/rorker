746acf5bb7de1e53421b8fe577cbcad8e90f8c8c
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-09-14 01:26:08 +0900

    add test_rorker_init

--- test/rorker_test.rb
+++ test/rorker_test.rb
@@ -6,4 +6,10 @@ class RorkerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Rorker::VERSION
   end
+
+  def test_rorker_init
+    task = Ractor.new{}
+    rorker = Rorker.new(task)
+    assert_kind_of Rorker, rorker
+  end
 end
