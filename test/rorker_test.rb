ecca45310b93b2fba3c6ea2e3770b83e2798c6f6
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-09-14 01:18:24 +0900

    remove test_it_does_something_useful

--- test/rorker_test.rb
+++ test/rorker_test.rb
@@ -6,8 +6,4 @@ class RorkerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Rorker::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
